d929a22178e4ad6124846db8c2e8fc41df85e09b
Kalpana Thakur <kalpanathakur6636@gmail.com>
2026-03-05 07:58:52 +0530

    switch.cpp

---
+++ switch.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num = 8;
+    switch(num){
+        case 1: cout<<"First number"<<endl;
+        break;
+        case 2:cout<<"Second"<<endl;
+        break;
+        default :cout<<"no one "<<endl;
+    }
+    
+}
